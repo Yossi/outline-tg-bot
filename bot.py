2ce68f8fb8d4751be3ff344ab54cb0bcea857fa7
Yossi Rafelson <yossi.rafelson@gmail.com>
2025-09-15 17:10:11 -0700

    Make archive.is hit more often
    Probably. Maybe.

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 '''Telegram bot that (primarily) attempts to perform url hacks to get around paywalls'''
 
 
-__version__ = '2.6.2'
+__version__ = '2.7.0'
 
 
 import asyncio
@@ -336,13 +336,20 @@ async def twelve_ft(url: str, client: httpx.AsyncClient) -> str | None:
 async def archive_is(url: str, client: httpx.AsyncClient) -> str | None:
     '''Returns the url for this page at archive.is if it exists'''
     # List of TLDs they have: .is .ph .md .li .vn .fo .today
-    try:
-        headers = {'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/114.0'}
-        r = await client.get(f'https://archive.today/timemap/{url}', timeout=2, headers=headers)
-        if r.status_code == 200:
-            return f'https://archive.is/newest/{url}'
-    except httpx.TimeoutException:
-        pass
+    headers = {'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/114.0'}
+    async def check_archive_is(url: str) -> str | None:
+        try:
+            r = await client.get(f'https://archive.is/timemap/{url}', timeout=2, headers=headers)
+            if r.status_code == 200:
+                return f'https://archive.is/newest/{url}'
+        except httpx.TimeoutException:
+            pass
+
+    archive_is_url = await check_archive_is(url)
+    if not archive_is_url:
+        url = urlsplit(url)._replace(query='').geturl()  # Strip query to maybe canonicalize url
+        archive_is_url = await check_archive_is(url)
+    return archive_is_url
 
 
 @timer
